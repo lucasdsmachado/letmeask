cb06e56c39d68f2e74a56f41745098c1245caeba
lucasdsmachado <briandsmachado@gmail.com>
2022-01-25 14:33:36 -0300

    create env variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPsStiG6il_Yg-cCuON0h8IbnH8hEpDDY",
-  authDomain: "letmeask-9f55a.firebaseapp.com",
-  databaseURL: "https://letmeask-9f55a-default-rtdb.firebaseio.com",
-  projectId: "letmeask-9f55a",
-  storageBucket: "letmeask-9f55a.appspot.com",
-  messagingSenderId: "45929310691",
-  appId: "1:45929310691:web:f32b58dd5d469e2dbf58d6"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
